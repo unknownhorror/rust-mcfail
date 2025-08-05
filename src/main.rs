cd1ad5b0a897e78248838602891493bed43f6f72
anarchaos <seminokoinzer406@gmail.com>
2025-08-05 19:13:49 +0200

    refactor to async (im praying its faster)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use ipnet;
+use ipnet::Ipv4Net;
+use std::str::FromStr;
+use std::sync::{Arc, Mutex};
 
-fn check_ip_legality(checked_ip: &ipnet::Ipv4Net) -> bool {
+async fn check_ip_legality(checked_ip: &ipnet::Ipv4Net) -> bool {
     use ipnet::Ipv4Net;
 
     let excluded_ips: Vec<ipnet::Ipv4Net> = vec![
@@ -16,7 +19,7 @@ fn check_ip_legality(checked_ip: &ipnet::Ipv4Net) -> bool {
         "240.0.0.0/4".parse().unwrap(), // Reserved for future use
         // 📡 Multicast addresses (used for streaming, routing protocols, etc.
         "224.0.0.0/4".parse().unwrap(),
-    // 🧱 Internet infrastructure / non-game hosts (o^ptional
+        // 🧱 Internet infrastructure / non-game hosts (o^ptional
         "4.2.2.1/32".parse().unwrap(), // Level3 DNS
         "4.2.2.2/32".parse().unwrap(),
         "4.2.2.3/32".parse().unwrap(),
@@ -25,44 +28,54 @@ fn check_ip_legality(checked_ip: &ipnet::Ipv4Net) -> bool {
         "4.2.2.6/32".parse().unwrap(),
     ];
 
-
     !excluded_ips.iter().any(|ips| ips.contains(checked_ip))
 }
-fn generate_ips() -> Vec<ipnet::Ipv4Net> {
-    let mut ip_ranges: Vec<ipnet::Ipv4Net> = vec![];
+
+async fn generate_ips() -> Vec<ipnet::Ipv4Net> {
+    let mut ip_ranges: Arc<Mutex<Vec<Ipv4Net>>> = Default::default();
+    let mut handles = vec![];
     for i in 0..256 {
         for j in 0..256 {
-            let ip: ipnet::Ipv4Net = format!("{i}.{j}.0.0/16").parse().unwrap();
-            if check_ip_legality(&ip) {
-                ip_ranges.push(ip);
-            }
+            let ip = format!("{}.{}.0.0/16", i, j)
+                .parse::<ipnet::Ipv4Net>()
+                .unwrap();
+            let ip_ranges = ip_ranges.clone();
+            handles.push(tokio::spawn(async move {
+                if (check_ip_legality(&ip).await) {
+                    ip_ranges.lock().unwrap().push(ip);
+                }
+            }));
         }
     }
-    ip_ranges
+    let mut sol: Vec<Ipv4Net> = vec![];
+    for handle in handles {
+        tokio::join!(handle);
+    }
+    for i in ip_ranges.lock().unwrap().iter() {
+        sol.push(*i);
+    }
+    sol
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let ips = generate_ips();
-    println!("ips:\n{:#?}", ips.len());
+    println!("ips:\n{:#?}", ips.await);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn generate_full_ip_set() {
-        assert_eq!(generate_ips().len(), 56558)
+    #[tokio::test]
+    async fn generate_full_ip_set() {
+        assert_eq!(generate_ips().await.len(), 56558)
     }
 
-    #[test]
-    fn deny_localhost() {
+    #[tokio::test]
+    async fn deny_localhost() {
         for i in 0..256 {
-            assert!(!check_ip_legality(
-                &format!("127.{i}.0.0/16").parse().unwrap()
-            ));
+            assert!(!check_ip_legality(&format!("127.{i}.0.0/16").parse().unwrap()).await);
         }
     }
-
-    }
-
+}
